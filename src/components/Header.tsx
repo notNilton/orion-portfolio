b10cd48650a6aff732470a2c40069a6b7fc5310a
Nilton Santos <nilton.naab@gmail.com>
2024-09-12 15:49:45 -0400

    refactor: change on url logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,30 +2,48 @@ import React, { useEffect, useState } from 'react';
 import "../styles/Header.scss";
 
 const Header: React.FC = () => {
-  const [activePage, setActivePage] = useState<string>("");
+  const [activePage, setActivePage] = useState<string>(() => {
+    // Tenta pegar a página ativa do localStorage, se não houver, define como 'projects' por padrão
+    return localStorage.getItem("activePage") || "projects";
+  });
 
   useEffect(() => {
-    // Pega o caminho atual da URL
-    const path = window.location.pathname;
-    setActivePage(path);
-  }, []);
+    // Atualiza o localStorage sempre que a página ativa mudar
+    localStorage.setItem("activePage", activePage);
+  }, [activePage]);
+
+  const handlePageChange = (page: string) => {
+    setActivePage(page);
+  };
 
   return (
     <header className="header">
       <h1>Nilton Santos</h1>
       <nav className="subject-selector">
         <ul>
-          <li className={activePage === "/projects" ? "active" : ""}>
-            <a href="/projects">Projects</a>
+          <li
+            className={activePage === "projects" ? "active" : ""}
+            onClick={() => handlePageChange("projects")}
+          >
+            Projects
           </li>
-          <li className={activePage === "/curriculum" ? "active" : ""}>
-            <a href="/curriculum">Curriculum</a>
+          <li
+            className={activePage === "curriculum" ? "active" : ""}
+            onClick={() => handlePageChange("curriculum")}
+          >
+            Curriculum
           </li>
-          <li className={activePage === "/inspirations" ? "active" : ""}>
-            <a href="/inspirations">Inspirations</a>
+          <li
+            className={activePage === "inspirations" ? "active" : ""}
+            onClick={() => handlePageChange("inspirations")}
+          >
+            Inspirations
           </li>
-          <li className={activePage === "/sobre" ? "active" : ""}>
-            <a href="/sobre">About</a>
+          <li
+            className={activePage === "sobre" ? "active" : ""}
+            onClick={() => handlePageChange("sobre")}
+          >
+            About
           </li>
         </ul>
       </nav>
