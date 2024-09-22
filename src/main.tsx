8680de174625d30226c4aa94fc024684dbdd294f
Nilton Santos <nilton.naab@gmail.com>
2024-09-21 22:00:54 -0400

    refactor: change main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './styles/Global.scss'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App'; // Não há necessidade de incluir '.tsx'
+import './styles/Global.scss';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function renderApp() {
+  const rootElement = document.getElementById('root');
+  
+  if (rootElement) {
+    createRoot(rootElement).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  } else {
+    console.error('Root element not found! Make sure the element with id="root" is present in your index.html.');
+  }
+}
+
+// Chama a função para inicializar a aplicação
+renderApp();
