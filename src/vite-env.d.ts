5ac6cfe3f553b4cffdb0fcb31ede39725b0a7911
Nilton Santos <nilton.naab@gmail.com>
2024-09-21 21:59:19 -0400

    chore: update vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    // Outras variáveis de ambiente personalizadas podem ser declaradas aqui
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
