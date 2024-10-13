28bdc4d6c4c2ea2f409d966ce3afe9e229a90374
Nilton Santos <nilton.naab@gmail.com>
2024-10-13 18:05:51 -0400

    refactor: improve Curriculum code

--- src/components/content/Curriculum.tsx
+++ src/components/content/Curriculum.tsx
@@ -1,113 +1,152 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import "../../styles/content/Curriculum.scss"; // Importando os estilos customizados
+
+// Definir dados para seções
+const educationData = [
+  {
+    degree: "Bacharelado em Engenharia de Computação",
+    institution: "Universidade Federal de Mato Grosso",
+    period: "2020 - 2025",
+  },
+];
+
+const workExperienceData = [
+  {
+    title: "Desenvolvedor Junior Full Stack",
+    company: "PGE-MT",
+    period: "Maio 2024 - Atual",
+    description: `
+      Implementei automações com Google Apps Script e AppSheet, otimizando fluxos internos. 
+      Desenvolvi sistemas de gerenciamento de processos jurídicos e controle de escalas para 
+      procuradores usando TypeScript e React no front-end, PostgreSQL como banco de dados, e deploy 
+      na Firebase com autenticação via Google Authentication.
+    `,
+    tasks: [
+      "Integração do ChatGPT para resumos automáticos de processos e pré-avaliação de litispendência.",
+    ],
+    technologies: [
+      "TypeScript",
+      "React",
+      "PostgreSQL",
+      "Firebase",
+      "Node.js",
+      "Google Apps Script",
+      "AppSheet",
+      "ChatGPT API",
+    ],
+  },
+  {
+    title: "Estágio Desenvolvimento Back-end",
+    company: "SEMA-MT",
+    period: "Maio 2021 - Maio 2022",
+    description: `
+      Desenvolvi uma ferramenta de campo utilizando C# e .NET para coletar e gerenciar dados 
+      em ambientes remotos. Implementei a sincronização de dados tanto online quanto offline, 
+      garantindo a consistência dos dados.
+    `,
+    tasks: [
+      "Integração com sistemas internos e externos por meio de APIs robustas.",
+    ],
+    technologies: ["C#", ".NET", "SQL Server", "APIs RESTful"],
+  },
+];
+
+const projectsData = [
+  {
+    name: "RADARE",
+    description: `
+      Aplicativo web em React para reconciliação de dados industriais, capaz de reconciliar 
+      grandes volumes de dados em tempo real ou batches, ajustando variáveis para valores corretos.
+    `,
+    technologies: ["Python (Flask)", "TypeScript (React)", "PostgreSQL (TimescaleDB)"],
+  },
+  {
+    name: "AAICAP",
+    description: `
+      Projeto para correção de imagens pixel-art geradas por Inteligência Artificial, utilizando 
+      técnicas avançadas de processamento de imagens para corrigir e otimizar a qualidade.
+    `,
+    technologies: ["Python", "Flask", "OpenCV", "PIL"],
+  },
+];
+
+const skillsData = {
+  languages: [
+    "Português (nativo)",
+    "Inglês (fluente)",
+    "Espanhol (mediano)",
+    "Francês (iniciante)",
+    "Alemão (iniciante)",
+  ],
+  programmingLanguages: ["C/C++", "Python", "TypeScript"],
+  frameworks: ["ReactJS", "Flask", "Express.js", "FastAPI"],
+  databases: ["Firebase", "PostgreSQL", "TimescaleDB"],
+};
 
 const Curriculum: React.FC = () => {
   return (
     <div className="curriculum">
       <div className="resume-content">
-        <section className="education section-aligned">
-          <h2>Educação</h2>
-          <ul>
-            <li>
-              <strong>Bacharelado em Engenharia de Computação</strong> -
-              Universidade Federal de Mato Grosso (2020 - 2025)
-            </li>
-          </ul>
-        </section>
 
-        <section className="work-experience section-aligned">
-          <h2>Experiência Profissional</h2>
-          <ul>
-            <li>
-              <strong>Desenvolvedor Junior Full Stack</strong> - PGE-MT (Maio
-              2024 - Atual)
-              <p>
-                Implementei automações com Google Apps Script e AppSheet,
-                otimizando fluxos internos. Desenvolvi sistemas de gerenciamento
-                de processos jurídicos e controle de escalas para procuradores
-                usando TypeScript e React no front-end, PostgreSQL como banco de
-                dados, e deploy na Firebase com autenticação via Google
-                Authentication.
-              </p>
-              <ul>
-                <li>
-                  Integração do ChatGPT para resumos automáticos de processos e
-                  pré-avaliação de litispendência.
-                </li>
-                <li>
-                  Tecnologias: TypeScript, React, PostgreSQL, Firebase, Node.js,
-                  Google Apps Script, AppSheet, ChatGPT API.
-                </li>
-              </ul>
+        {/* Seção Educação */}
+        <Section title="Educação">
+          {educationData.map((edu, index) => (
+            <li key={index}>
+              <strong>{edu.degree}</strong> - {edu.institution} ({edu.period})
             </li>
-            <li>
-              <strong>Estágio Desenvolvimento Back-end</strong> - SEMA-MT (Maio
-              2021 - Maio 2022)
-              <p>
-                Desenvolvi uma ferramenta de campo utilizando C# e .NET para
-                coletar e gerenciar dados em ambientes remotos. Implementei a
-                sincronização de dados tanto online quanto offline, garantindo a
-                consistência dos dados.
-              </p>
-              <ul>
-                <li>
-                  Integração com sistemas internos e externos por meio de APIs
-                  robustas.
-                </li>
-                <li>Tecnologias: C#, .NET, SQL Server, APIs RESTful.</li>
-              </ul>
-            </li>
-          </ul>
-        </section>
+          ))}
+        </Section>
 
-        <section className="projects section-aligned">
-          <h2>Projetos Pessoais</h2>
-          <ul>
-            <li>
-              <strong>RADARE</strong> - Reconciliation and Data Analysis in a
-              Responsive Environment
-              <p>
-                Aplicativo web em React para reconciliação de dados industriais,
-                capaz de reconciliar grandes volumes de dados em tempo real ou
-                batches, ajustando variáveis para valores corretos.
-              </p>
+        {/* Seção Experiência Profissional */}
+        <Section title="Experiência Profissional">
+          {workExperienceData.map((job, index) => (
+            <li key={index}>
+              <strong>{job.title}</strong> - {job.company} ({job.period})
+              <p>{job.description}</p>
               <ul>
+                {job.tasks.map((task, taskIndex) => (
+                  <li key={taskIndex}>{task}</li>
+                ))}
                 <li>
-                  Tecnologias: Python (Flask), TypeScript (React), PostgreSQL
-                  (TimescaleDB).
+                  Tecnologias: {job.technologies.join(", ")}
                 </li>
               </ul>
             </li>
-            <li>
-              <strong>AAICAP</strong> - Artificial Intelligence Correction and
-              Augmentation Pipeline
-              <p>
-                Projeto para correção de imagens pixel-art geradas por
-                Inteligência Artificial, utilizando técnicas avançadas de
-                processamento de imagens para corrigir e otimizar a qualidade.
-              </p>
+          ))}
+        </Section>
+
+        {/* Seção Projetos Pessoais */}
+        <Section title="Projetos Pessoais">
+          {projectsData.map((project, index) => (
+            <li key={index}>
+              <strong>{project.name}</strong>
+              <p>{project.description}</p>
               <ul>
-                <li>Tecnologias: Python, Flask, OpenCV, PIL.</li>
+                <li>Tecnologias: {project.technologies.join(", ")}</li>
               </ul>
             </li>
-          </ul>
-        </section>
+          ))}
+        </Section>
 
-        <section className="skills section-aligned">
-          <h2>Habilidades Técnicas</h2>
-          <ul>
-            <li>
-              Idiomas: Português (nativo), Inglês (fluente), Espanhol (mediano),
-              Francês (iniciante), Alemão (iniciante)
-            </li>
-            <li>Linguagens: C/C++, Python, TypeScript</li>
-            <li>Frameworks: ReactJS, Flask, Express.js, FastAPI</li>
-            <li>Bancos de Dados: Firebase, PostgreSQL, TimescaleDB</li>
-          </ul>
-        </section>
+        {/* Seção Habilidades Técnicas */}
+        <Section title="Habilidades Técnicas">
+          <li>Idiomas: {skillsData.languages.join(", ")}</li>
+          <li>Linguagens: {skillsData.programmingLanguages.join(", ")}</li>
+          <li>Frameworks: {skillsData.frameworks.join(", ")}</li>
+          <li>Bancos de Dados: {skillsData.databases.join(", ")}</li>
+        </Section>
+
       </div>
     </div>
   );
 };
+
+// Componente Section para evitar repetição de código
+const Section: React.FC<{ title: string; children: ReactNode }> = ({ title, children }) => (
+  <section className="section-aligned">
+    <h2>{title}</h2>
+    <ul>{children}</ul>
+  </section>
+);
 
 export default Curriculum;
